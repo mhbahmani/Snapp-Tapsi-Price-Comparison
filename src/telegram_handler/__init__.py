328f9f08e879d4dd94e5bbd8b2eace7eaf49d70e
mhbahmani <mhbahmani79@gmail.com>
2023-08-10 11:50:18 +0330

    Add telegram_handler package

---
+++ src/telegram_handler/__init__.py
@@ -0,0 +1,1 @@
+from src.telegram_handler.telegram import *
---
+++ src/telegram_handler/telegram.py
@@ -0,0 +1,27 @@
+from decouple import config
+
+import telegram
+import datetime
+
+
+class Telegram:
+    def __init__(self) -> None:
+        self.CHANNEL_NAME = "Snapp/Tapsi Price Comparison"
+        self.CHANNEL_ID = -1001945780088
+
+        self.bot = telegram.Bot(
+            token=config("TELEGRAM_BOT_TOKEN")
+        )
+
+    def send_report(self, text: str, medias: list):
+        self.bot.send_media_group(
+            chat_id=self.CHANNEL_ID,
+            media=[
+                telegram.InputMediaPhoto(
+                    media=open(media_path, 'rb'),
+                    caption=text if i == 0 else None,
+                )
+                for i, media_path in enumerate(medias)
+            ],
+        )
+        print(f"Report has been sent at {datetime.datetime.now()}")
