2a7d3c22894e52e6ba890e2d950195463b07d88d
mhbahmani <mhbahmani79@gmail.com>
2023-08-10 16:51:40 +0330

    Add new panels to report

--- price_reporter.py
+++ price_reporter.py
@@ -12,8 +12,32 @@ panels = [
     {
         "panel_id": 3,
         "dashboard_id": "d90a5e73-63d1-43d9-9f81-e776ba7e0c31",
-        "message": "میانگین قیمت اسنپ و تپسی در ۲۴ ساعت گذشته {}",
-        "description": "Average panel"
+        "message": "میانگین قیمت اسنپ و تپسی در ۲۴ ساعت گذشته (سفر عادی)",
+        "description": "Average panel (normal)"
+    },
+    {
+        "panel_id": 43,
+        "dashboard_id": "d90a5e73-63d1-43d9-9f81-e776ba7e0c31",
+        "message": "میانگین قیمت اسنپ و تپسی در ۲۴ ساعت گذشته (عجله دارم)",
+        "description": "Average panel (in hurry)"
+    },
+    {
+        "panel_id": 54,
+        "dashboard_id": "d90a5e73-63d1-43d9-9f81-e776ba7e0c31",
+        "message": "بیش‌ترین و کم‌ترین اختلاف قیمت حالت عجله دارم و عجله ندارم در بین تمامی مسیرها ",
+        "description": "Diff Max/Min"
+    },
+    {
+        "panel_id": 39,
+        "dashboard_id": "d90a5e73-63d1-43d9-9f81-e776ba7e0c31",
+        "message": "قیمت Tapsi در مسیرهایی که تا به حال نرفته‌ام (حالت عادی)",
+        "description": "Not Gone Routes tapsi normal"
+    },
+    {
+        "panel_id": 50,
+        "dashboard_id": "d90a5e73-63d1-43d9-9f81-e776ba7e0c31",
+        "message": "قیمت Snapp در مسیرهایی که تا به حال نرفته‌ام (حالت عادی)",
+        "description": "Not Gone Routes snapp normal"
     },
 ]
 
@@ -23,14 +47,16 @@ def report_average_panel_image():
     telegram = Telegram()
     grafana = Grafana()
 
+    telegram.send_message(f"گزارش قیمت‌ها در ۲۴ ساعت گذشته {jdatetime.date.today()}")
+
     for panel in panels:
         panel_image_file_path = generate_today_panel_image_file_name(panel["panel_id"])
         grafana.download_panel_image(output_file_path=panel_image_file_path, panel_id=panel["panel_id"], dashboard_id=panel["dashboard_id"])
 
         medias = [panel_image_file_path]
 
-        # twitter.send_tweet(panel["message"].format(jdatetime.date.today()), medias)
-        telegram.send_report(panel["message"].format(jdatetime.date.today()), medias)
+        # twitter.send_tweet(panel["message"], medias)
+        telegram.send_report(panel["message"], medias)
 
 # report_average_panel_image()
 
