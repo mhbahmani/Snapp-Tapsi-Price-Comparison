f5789a9044a53a500b0e5687c3f334c5925cb3d0
mhbahmani <mhbahmani79@gmail.com>
2023-08-28 01:47:52 +0330

    Add use backup server handler script

--- get_prices_from_backup_server.sh
+++ get_prices_from_backup_server.sh
@@ -2,14 +2,13 @@
 
 log_file="price_fetcher.logs"
 # target_line="Timeout, Waiting..."
-target_line="referenced before assignment"
-target_line_2="Connection refused"
+target_line="Use backup server"
 
 # Continuously monitor the log file
 tail -F -n0 "$log_file" | while read -r line
 do
 	# Check if the target line is present
-	if [[ "$line" == *"$target_line"* ]] || [[ "$line" == *"$target_line_2"* ]]; then
+	if [[ "$line" == *"$target_line"* ]]; then
 		echo "\"$line\" Fount. Performing price copy"
 
 		ssh -l root dgocean-1 "cd /root/projects/price_com/; PYTHONUNBUFFERED=0 SCRAPE_INTERVAL=600 python3 /root/projects/price_com/price_fetcher.py | tee -a /root/projects/price_com/price_fetcher.logs"
