bb0708b9bd40e7cf52ff3aaca77c684a0a011d9f
mhbahmani <mhbahmani79@gmail.com>
2023-08-28 02:10:10 +0330

    Add RUN_ONCE feature

--- price_fetcher.py
+++ price_fetcher.py
@@ -3,63 +3,75 @@ from src.tapsi import Tapsi
 from src.metrics_handler import MetricsHandler
 from src.utils import load_routes
 
+from decouple import config 
+
 from time import sleep
 
-import os
 
-
-SCRAPE_INTERVAL = int(os.getenv("SCRAPE_INTERVAL", 300))
+SCRAPE_INTERVAL = int(config("SCRAPE_INTERVAL", 300, cast=int))
 
 snapp = Snapp()
 tapsi = Tapsi()
 exporter = MetricsHandler("./prices.txt")
 
 
-if __name__ == "__main__":
-    print(f"Scrape Interval: {SCRAPE_INTERVAL}")
-    routes = load_routes()
+def fetch_prices(routes: list):
+    prices = []
+    for in_hurry in [False, True]:
+        try:
+            for route in routes:
+                print("SNAPP", route["tag"], "IN_HURRY:", in_hurry)
+                prices.append(
+                    {
+                        "provider": "snapp",
+                        "route": route["tag"],
+                        "price": snapp.get_route_price(
+                            route["source"],
+                            route["destination"],
+                            in_hurry=in_hurry),
+                        "in_hurry": in_hurry
+                    }
+                )
+                sleep(1)
 
-    while True:
-        prices = []
-        for in_hurry in [False, True]:
-            try:
-                for route in routes:
-                    print("SNAPP", route["tag"], "IN_HURRY:", in_hurry)
-                    prices.append(
-                        {
-                            "provider": "snapp",
-                            "route": route["tag"],
-                            "price": snapp.get_route_price(
-                                route["source"],
-                                route["destination"],
-                                in_hurry=in_hurry),
-                            "in_hurry": in_hurry
-                        }
-                    )
-                    sleep(1)
-
-                for route in routes:
-                    print("TAPSI", route["tag"], "IN_HURRY:", in_hurry)
-                    prices.append(
-                        {
-                            "provider": "tapsi",
-                            "route": route["tag"],
-                            "price": tapsi.get_route_price(
-                                route["source"],
-                                route["destination"],
-                                in_hurry=in_hurry),
-                            "in_hurry": in_hurry
-                        }
-                    )
-                    sleep(1)
-            except Exception as e:
-                print(e)
-                print("Use backup server")
-                sleep(SCRAPE_INTERVAL)
-                continue
-            
+            for route in routes:
+                print("TAPSI", route["tag"], "IN_HURRY:", in_hurry)
+                prices.append(
+                    {
+                        "provider": "tapsi",
+                        "route": route["tag"],
+                        "price": tapsi.get_route_price(
+                            route["source"],
+                            route["destination"],
+                            in_hurry=in_hurry),
+                        "in_hurry": in_hurry
+                    }
+                )
+                sleep(1)
+        except Exception as e:
+            print(e)
+            print("Use backup server")
+            break
+    else:
         exporter.parse_prices_into_metrics(prices)
         exporter.export_metrics()
-        print("done")
-        # print(prices)
+        
+    print("Done")
+    # print(prices)
+
+
+if __name__ == "__main__":
+    routes = load_routes()
+
+    # Set a flag to pass to code on running command in order to bypass while loop
+    # and run the code once
+    if config("RUN_ONCE", False, cast=bool):
+        print("RUN_ONCE is set to True, running once")
+        fetch_prices(routes)
+
+    print(f"Scrape Interval: {SCRAPE_INTERVAL}")
+
+    while True:
+        fetch_prices(routes)
         sleep(SCRAPE_INTERVAL)
+
